26b6d81513f0f8fa0fdd61ecbafe56ef015df2d1
Marco Rios <marco@marcorios.me>
2021-07-19 00:00:32 +0800

    init

--- src/services/artist.js
+++ src/services/artist.js
@@ -7,14 +7,49 @@ export const artist = async (req, res, next) => {
         
         const { data } = await infoArtist(query.mbid);
         
-        const response = data;
+        const artist = data;
+
+        const string = JSON.stringify(artist);
+
+        const Rs = JSON.parse(string);
+
+        //console.log(Rs.type);
+
+        const response = {
+            code: 1,
+            status: 'sucess',
+            message: 'sucess',
+            response: {
+                id: Rs.id, 
+                typeId: Rs["type-id"],
+                name: Rs.name,
+                sortName: Rs["sort-name"],
+                type: Rs.type,
+                gender: Rs.gender,
+                disambiguation: Rs.disambiguation, 
+                genderId: Rs["gender-id"],
+                country: Rs.country,
+                area: Rs.area,
+                genres: Rs.genres,
+                lifeSpan: Rs["life-span"],
+                tags: Rs.tags,
+                relations: Rs.relations,
+                begin_area: Rs.begin_area,
+                beginArea: Rs["begin-area"],
+                end_area: Rs["end_area"],
+                endArea: Rs["end-area"],
+                isnis: Rs.isnis,
+                ipis: Rs.ipis, 
+                annotation: Rs.annotation
+            }
+        };
 
         res.status(status.OK).send(response);
 
     } catch (error) {
 
         next(error);
-        console.log("Error: " + err.message);
+        console.log("Error: " + error);
 
     }
     
