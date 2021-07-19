b62c22bf0f0c0631723cfac662a5b38de55e1841
Marco Rios <marco@marcorios.me>
2021-07-19 17:30:58 +0800

    init

--- src/services/place.js
+++ src/services/place.js
@@ -17,33 +17,7 @@ export const place = async (req, res, next) => {
             code: 1,
             status: 'sucess',
             message: 'sucess',
-            response: {
-                id: Rs.id, 
-                name: Rs.name,
-                sortName: Rs["sort-name"],
-                aliases: Rs.aliases,
-                type: Rs.type,
-                typeId: Rs["type-id"],
-                gender: Rs.gender,
-                genderId: Rs["gender-id"],
-                disambiguation: Rs.disambiguation, 
-                country: Rs.country,
-                area: Rs.area,
-                genres: Rs.genres,
-                releases: Rs.releases,
-                releaseGroups: Rs["release-groups"],
-                recordings: Rs.recordings,
-                lifeSpan: Rs["life-span"],
-                tags: Rs.tags,
-                relations: Rs.relations,
-                begin_area: Rs.begin_area,
-                beginArea: Rs["begin-area"],
-                end_area: Rs["end_area"],
-                endArea: Rs["end-area"],
-                isnis: Rs.isnis,
-                ipis: Rs.ipis, 
-                annotation: Rs.annotation
-            }
+            response: Rs
         };
 
         res.status(status.OK).send(response);
